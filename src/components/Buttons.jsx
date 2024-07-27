7772a9f35e53958c58ae2772fb17e0db7fc00a4a
Vansh <vanshsharma9933@gmail.com>
2024-07-27 20:57:28 +0530

    update images css

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,51 +3,51 @@ import React from "react";
 function Buttons() {
   return (
     <div className="grid grid-cols-3 gap-4 p-4 max-w-screen-md mx-auto text-2xl">
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-happy">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-happy emotion-happy">
         <div className="bg-overlay" />
         <span className="text-overlay">Happy</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-sad">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-sad emotion-sad">
         <div className="bg-overlay" />
         <span className="text-overlay">Sad</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-emotional">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-emotional emotion-emotional">
         <div className="bg-overlay" />
         <span className="text-overlay">Emotional</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-tired">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-tired emotion-tired">
         <div className="bg-overlay" />
         <span className="text-overlay">Tired</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-angry">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-angry emotion-angry">
         <div className="bg-overlay" />
         <span className="text-overlay">Angry</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-romantic">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-romantic emotion-romantic">
         <div className="bg-overlay" />
         <span className="text-overlay">Romantic</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-horny">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-horny emotion-horny">
         <div className="bg-overlay" />
         <span className="text-overlay">Horny</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-energetic">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-energetic emotion-energetic">
         <div className="bg-overlay" />
         <span className="text-overlay">Energetic</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-chill">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-chill emotion-chill">
         <div className="bg-overlay" />
         <span className="text-overlay">Chill</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-cheerful">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-cheerful emotion-cheerful">
         <div className="bg-overlay" />
         <span className="text-overlay">Cheerful</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-thrillfull">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-thrillfull emotion-thrillfull">
         <div className="bg-overlay" />
         <span className="text-overlay">Thrillfull</span>
       </button>
-      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-humorous">
+      <button className="relative text-gray-100 font-bold py-4 px-8 rounded-lg overflow-hidden bg-humorous emotion-humorous">
         <div className="bg-overlay" />
         <span className="text-overlay">Humorous</span>
       </button>
