11a4a3c8e039237afaf4709643489193908fe36c
Vansh <vanshsharma9933@gmail.com>
2024-07-27 19:54:33 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Example() {
+  return (
+    <div>
+      <div>
+        <nav className="bg-gray-800">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-center">
+              <h1 className="text-white text-2xl font-bold font-exo2 text-3xl">
+                Welcome to Bollymood!
+              </h1>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className="flex items-center justify-center p-5">
+        <h1 className="text-center text-4xl">
+          Discover top-rated Bollywood movies<br />based on your mood
+        </h1>
+      </div>
+    </div>
+  );
+}
