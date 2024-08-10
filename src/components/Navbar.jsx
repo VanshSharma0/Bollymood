d9629df17277ff4ef9a1c158bb81a98c3021c57f
vanshsharma <vanshsharma9933@gmail.com>
2024-08-10 23:29:26 +0530

    fix responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ export default function Navbar() {
   return (
     <div>
       <div>
-        <nav className="bg-black-600 underline text-white text-4xl">
+        <nav className="bg-black-600 underline text-white">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-center">
-              <h1 className="text-white font-bold font-exo2 text-4xl sm:text-3xl">
+              <h1 className="text-white font-bold font-exo2 text-3xl sm:text-3xl mt-2">
                 Welcome to Bollymood!
               </h1>
             </div>
           </div>
         </nav>
       </div>
-      <div className="flex flex-col items-center justify-center p-5 text-center">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">
+      <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl text-white lg:text-3xl md:text-2xl">
           Discover top-rated Bollywood movies<br />based on your mood
         </h1>
         <br />
